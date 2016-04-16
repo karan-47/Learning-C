0b6c37c099c08f8d6d42a3e6b4172d6fed0ca74d
Karan Gala <galakaran47@gmail.com>
2016-04-16 22:16:10 +0530

    Adding code for area of triangle

---
+++ AreaOfTriangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    float a,b,h;
+    printf("enter the base\n");
+    scanf("%f",&b);
+    printf("enter the height\n");
+    scanf("%f",&h);
+    a=0.5*b*h;
+    printf("the area of the triangle is\n %f",a);
+
+
+    
+
+    return 0;
+}
