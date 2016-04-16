a7fee00bdd276c1ae6c6e6617e688267a17d5a6a
Karan Gala <galakaran47@gmail.com>
2016-04-16 22:30:32 +0530

    Adding code for area of circle

---
+++ AreaOfCircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main(){
+    float a,r;
+    printf("enter the radius\n");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("the area of the circle is\n %f",a);
+
+    return 0;
+    }
