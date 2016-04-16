60ce08e8799a0ba305027c3b54d0a061d9bedacd
karan-47 <galakaran47@gmail.com>
2016-04-16 20:21:38 +0530

    Adding Centigrade to Fahrenheit conversion program in C.

---
+++ CtoF.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+int main(void)
+
+{
+    
+float C,F;
+    
+printf("enter temperature in C\n");
+    
+scanf("%f",&C);
+    
+F=5*C/9 +32;
+    
+printf("the temperature in F is\n %f",F);
+
+    
+return 0;
+
+}
+
